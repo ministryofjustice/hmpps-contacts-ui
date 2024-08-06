2ad8eb049f485779bfdb4ee5a48da3061f166458
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-08-06 11:27:22 +0100

    jQuery lib added to set up javascript for govuk and moj toolkit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
+import $ from "jquery"
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 
+window.$ = $
 govukFrontend.initAll()
 mojFrontend.initAll()
