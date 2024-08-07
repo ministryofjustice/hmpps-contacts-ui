d7b285f8b12359220d192ae67d1a6d4ba2e7f23f
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-08-07 13:51:38 +0100

    Inject validation error into njk view

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -6,6 +6,7 @@ import fs from 'fs'
 import { initialiseName } from './utils'
 import config from '../config'
 import logger from '../../logger'
+import { buildErrorSummaryList, findError } from '../middleware/validationMiddleware'
 
 export default function nunjucksSetup(app: express.Express): void {
   app.set('view engine', 'njk')
@@ -39,4 +40,6 @@ export default function nunjucksSetup(app: express.Express): void {
 
   njkEnv.addFilter('initialiseName', initialiseName)
   njkEnv.addFilter('assetMap', (url: string) => assetManifest[url] || url)
+  njkEnv.addFilter('buildErrorSummaryList', buildErrorSummaryList)
+  njkEnv.addFilter('findError', findError)
 }
