b526e978954d09fb828b2e06496f3e86421711d8
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-08-21 08:54:10 +0100

    add services

--- server/data/index.ts
+++ server/data/index.ts
@@ -16,6 +16,7 @@ import RedisTokenStore from './tokenStore/redisTokenStore'
 import InMemoryTokenStore from './tokenStore/inMemoryTokenStore'
 import config from '../config'
 import HmppsAuditClient from './hmppsAuditClient'
+import PrisonerSearchClient from './prisonerSearchClient'
 
 type RestClientBuilder<T> = (token: string) => T
 
@@ -25,8 +26,10 @@ export const dataAccess = () => ({
     config.redis.enabled ? new RedisTokenStore(createRedisClient()) : new InMemoryTokenStore(),
   ),
   hmppsAuditClient: new HmppsAuditClient(config.sqs.audit),
+  prisonerSearchClientBuilder: ((token: string) =>
+    new PrisonerSearchClient(token)) as RestClientBuilder<PrisonerSearchClient>,
 })
 
 export type DataAccess = ReturnType<typeof dataAccess>
 
-export { HmppsAuthClient, RestClientBuilder, HmppsAuditClient }
+export { HmppsAuthClient, RestClientBuilder, HmppsAuditClient, PrisonerSearchClient }
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,11 +5,11 @@ import SearchRoutes from './search/routes'
 import SEARCH_PRISONER_URL from './urls'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default function routes({ auditService }: Services): Router {
+export default function routes({ auditService, prisonerSearchService }: Services): Router {
   const router = Router({ mergeParams: true })
 
   router.use('/', ContactsRoutes(auditService))
-  router.use(SEARCH_PRISONER_URL, SearchRoutes(auditService))
+  router.use(SEARCH_PRISONER_URL, SearchRoutes(auditService, prisonerSearchService))
 
   return router
 }
