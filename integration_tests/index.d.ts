5b0b3b72bc64da69996e7f5adb95e9fbb580286f
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2024-09-02 15:22:57 +0100

    PRC-56 : Add assertions to create contact integration tests (#25)
    Assert that the create contact API is called with the correct parameters in the integration tests. While doing this I discovered a bug in that we were sending a title and middle name with an empty string instead of null.
    
    Also added some missing tests to check that you can't just enter whitespace for the name.

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -5,5 +5,12 @@ declare namespace Cypress {
      * @example cy.signIn({ failOnStatusCode: boolean })
      */
     signIn(options?: { failOnStatusCode: boolean }): Chainable<AUTWindow>
+
+    /**
+     * Custom command to verify that the last API call matching the parameter is deeply equal to the expected value.
+     * @param matching a wiremock request to /requests/find. For options see: https://wiremock.org/docs/standalone/admin-api-reference/#tag/Requests/operation/removeRequestsByMetadata
+     * @param expected the request body to match
+     */
+    verifyLastAPICall(matching: string | object, expected: object): Chainable<*>
   }
 }
