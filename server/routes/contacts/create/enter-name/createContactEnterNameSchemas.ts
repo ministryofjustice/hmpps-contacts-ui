18f61b2b76f899485ef60bd92de3854c6476171f
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2024-09-03 17:26:41 +0100

    PRC-71 : Complete name validation (#28)
    Added special char and number validation on names with a dedicated validation test for enter name.

--- server/routes/contacts/create/enter-name/createContactEnterNameSchemas.ts
+++ server/routes/contacts/create/enter-name/createContactEnterNameSchemas.ts
@@ -3,11 +3,16 @@ import { createSchema } from '../../../../middleware/validationMiddleware'
 
 const LAST_NAME_REQUIRED_MESSAGE = "Enter the contact's last name"
 const LAST_NAME_TOO_LONG_ERROR_MSG = "Contact's last name must be 35 characters or less"
+const LAST_NAME_INVALID = "Contact's last name must not contain special characters"
 
 const FIRST_NAME_REQUIRED_MESSAGE = "Enter the contact's first name"
 const FIRST_NAME_TOO_LONG_ERROR_MSG = "Contact's first name must be 35 characters or less"
+const FIRST_NAME_INVALID = "Contact's first name must not contain special characters"
 
 const MIDDLE_NAME_TOO_LONG_ERROR_MSG = "Contact's middle name must be 35 characters or less"
+const MIDDLE_NAME_INVALID = "Contact's middle name must not contain special characters"
+
+const NAME_REGEX = /^[a-zA-Z\s,.'-]*$/
 
 export const createContactEnterNameSchemaFactory = () => async () => {
   return createSchema({
@@ -19,15 +24,18 @@ export const createContactEnterNameSchemaFactory = () => async () => {
     lastName: z
       .string({ message: LAST_NAME_REQUIRED_MESSAGE })
       .max(35, LAST_NAME_TOO_LONG_ERROR_MSG)
+      .regex(NAME_REGEX, LAST_NAME_INVALID)
       .refine(val => val?.trim().length > 0, { message: LAST_NAME_REQUIRED_MESSAGE }),
     firstName: z
       .string({ message: FIRST_NAME_REQUIRED_MESSAGE })
       .max(35, FIRST_NAME_TOO_LONG_ERROR_MSG)
+      .regex(NAME_REGEX, FIRST_NAME_INVALID)
       .refine(val => val?.trim().length > 0, { message: FIRST_NAME_REQUIRED_MESSAGE })
       .transform(val => val?.trim()),
     middleName: z
       .string()
       .max(35, MIDDLE_NAME_TOO_LONG_ERROR_MSG)
+      .regex(NAME_REGEX, MIDDLE_NAME_INVALID)
       .optional()
       .transform(val => (val?.trim().length > 0 ? val.trim() : undefined)),
   })
