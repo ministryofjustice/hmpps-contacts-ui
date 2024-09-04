5f7eae89f37e49be48c46f2a4ee8d2fd534bb800
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-09-04 16:26:03 +0100

    undo code previously entered

--- integration_tests/pages/contacts.ts
+++ integration_tests/pages/contacts.ts
@@ -5,22 +5,9 @@ export default class ContactsPage extends Page {
     super('Contacts')
   }
 
-  enterSearchTerm(value: string): ContactsPage {
-    this.searchTermTextBox().type(value)
-    return this
-  }
-
-  clickContinue() {
-    this.continueButton().click()
-  }
-
   headerUserName = (): PageElement => cy.get('[data-qa=header-user-name]')
 
   manageContactsCard = (): PageElement => cy.get('[data-qa=manage-contacts-card]')
 
   manageContactRestrictionsCard = (): PageElement => cy.get('[data-qa=manage-restrictions-card]')
-
-  private searchTermTextBox = (): PageElement => cy.get('#search')
-
-  private continueButton = (): PageElement => cy.get('[data-test="search"]')
 }
