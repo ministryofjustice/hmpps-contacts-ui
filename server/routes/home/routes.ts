9c25859a227ba201005f4c65fac85775f7e90e89
james-baker-moj <159048573+james-baker-moj@users.noreply.github.com>
2024-09-05 15:59:00 +0100

    PRC-57: Wrap all controllers with asyncMiddleware (#36)
    All controllers that return a promise (which is all of them currently) should be wrapped in asyncMiddleware.
    This is because if they return an unresolved error in a promise it causes node to crash...
    This means that any unhandled 400 or 500 from a dependency would result in a crash.

--- server/routes/home/routes.ts
+++ server/routes/home/routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express'
 import HomeController from './controller'
 import logPageViewMiddleware from '../../middleware/logPageViewMiddleware'
 import AuditService from '../../services/auditService'
+import asyncMiddleware from '../../middleware/asyncMiddleware'
 
 const HomeRoutes = (auditService: AuditService): Router => {
   const router = Router({ mergeParams: true })
   const controller = new HomeController()
 
-  router.get('/', logPageViewMiddleware(auditService, controller), controller.GET)
+  router.get('/', logPageViewMiddleware(auditService, controller), asyncMiddleware(controller.GET))
   return router
 }
 
