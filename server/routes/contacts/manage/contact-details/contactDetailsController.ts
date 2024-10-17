e302ebde165fdee7e485e58a5dbdef3897b53e39
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-10-17 13:38:09 +0100

    Code commented removed from controller

--- server/routes/contacts/manage/contact-details/contactDetailsController.ts
+++ server/routes/contacts/manage/contact-details/contactDetailsController.ts
@@ -5,11 +5,10 @@ import { ContactsService } from '../../../../services'
 import ReferenceDataService from '../../../../services/referenceDataService'
 import ReferenceCodeType from '../../../../enumeration/referenceCodeType'
 import formatName from '../../../../utils/formatName'
-import ManageContactsJourney = journeys.ManageContactsJourney
+
 import PrisonerJourneyParams = journeys.PrisonerJourneyParams
 import Contact = contactsApiClientTypes.Contact
 import GetContactResponse = contactsApiClientTypes.GetContactResponse
-import logger from '../../../../../logger'
 
 export default class ContactDetailsController implements PageHandler {
   constructor(
@@ -19,38 +18,16 @@ export default class ContactDetailsController implements PageHandler {
 
   public PAGE_NAME = Page.CONTACT_DETAILS_PAGE
 
-  GET = async (
-    // req: Request<PrisonerJourneyParams, unknown, unknown, { contactId?: string }>,
-    req: Request<PrisonerJourneyParams, { journeyId?: string; contactId?: string }, unknown>,
-    res: Response,
-  ): Promise<void> => {
-    let journey: ManageContactsJourney
-    const { journeyId, contactId } = req.params
+  GET = async (req: Request<PrisonerJourneyParams, { contactId?: string }, unknown>, res: Response): Promise<void> => {
+    const { contactId } = req.params
     const { prisonerDetails, user } = res.locals
     const contact: Contact = await this.contactsService.getContact(parseInt(contactId, 10), user)
     const formattedFullName = await this.formattedFullName(contact, user)
-    logger.info(JSON.stringify(formattedFullName))
-
-    if (journeyId) {
-      journey = req.session.manageContactsJourneys[journeyId]
-      journey.prisoner = {
-        firstName: prisonerDetails?.firstName,
-        lastName: prisonerDetails?.lastName,
-        prisonerNumber: prisonerDetails?.prisonerNumber,
-        dateOfBirth: prisonerDetails?.dateOfBirth,
-        prisonName: prisonerDetails?.prisonName,
-      }
-
-      logger.info(JSON.stringify(contact))
-    }
 
     return res.render('pages/contacts/manage/contactDetails/details', {
       contact,
       prisonerDetails,
       formattedFullName,
-      // journey,
-      // isContactConfirmed: res.locals?.formResponses?.isContactConfirmed ?? journey?.isContactConfirmed,
-      // navigation: navigationForAddContactJourney(this.PAGE_NAME, journey),
     })
   }
 
