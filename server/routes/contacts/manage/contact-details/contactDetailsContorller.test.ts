0804173871874683bcca72a960a652cd32b7bd3a
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-10-29 12:01:16 +0000

    Commented code removed

--- server/routes/contacts/manage/contact-details/contactDetailsContorller.test.ts
+++ server/routes/contacts/manage/contact-details/contactDetailsContorller.test.ts
@@ -1,7 +1,5 @@
 import type { Express } from 'express'
-import { v4 as uuidv4 } from 'uuid'
 import request from 'supertest'
-import { SessionData } from 'express-session'
 import * as cheerio from 'cheerio'
 import { appWithAllRoutes, user } from '../../../testutils/appSetup'
 import AuditService, { Page } from '../../../../services/auditService'
@@ -9,7 +7,6 @@ import PrisonerSearchService from '../../../../services/prisonerSearchService'
 import ContactsService from '../../../../services/contactsService'
 import ReferenceDataService from '../../../../services/referenceDataService'
 import TestData from '../../../testutils/testData'
-import ManageContactsJourney = journeys.ManageContactsJourney
 
 jest.mock('../../../../services/auditService')
 jest.mock('../../../../services/prisonerSearchService')
@@ -22,27 +19,9 @@ const contactsService = new ContactsService(null) as jest.Mocked<ContactsService
 const referenceDataService = new ReferenceDataService(null) as jest.Mocked<ReferenceDataService>
 
 let app: Express
-// let session: Partial<SessionData>
 const prisonerNumber = 'A1234BC'
-// const journeyId: string = uuidv4()
-// let existingJourney: ManageContactsJourney
 
 beforeEach(() => {
-  // existingJourney = {
-  //   id: journeyId,
-  //   lastTouched: new Date().toISOString(),
-  //   prisoner: {
-  //     prisonerNumber: 'G4793VF',
-  //     lastName: 'Timothy',
-  //     firstName: 'Jack',
-  //     dateOfBirth: '',
-  //     prisonName: '',
-  //     cellLocation: '',
-  //   },
-  //   contactId: 23,
-  //   activateListPage: undefined,
-  //   inactivateListPage: undefined,
-  // }
   app = appWithAllRoutes({
     services: {
       auditService,
@@ -50,11 +29,6 @@ beforeEach(() => {
       contactsService,
       referenceDataService,
     },
-    // sessionReceiver: (receivedSession: Partial<SessionData>) => {
-    //   session = receivedSession
-    //   session.manageContactsJourneys = {}
-    //   session.manageContactsJourneys[journeyId] = existingJourney
-    // },
   })
   referenceDataService.getReferenceDescriptionForCode.mockResolvedValue('Mr')
 })
