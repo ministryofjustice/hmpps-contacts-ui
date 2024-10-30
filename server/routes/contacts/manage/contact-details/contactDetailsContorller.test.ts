0b4d7221e1836e6debfc81f4cf798f5a1f6485f9
James Baker <james.baker3@digital.justice.gov.uk>
2024-10-30 11:16:02 +0000

    PRC-210: Sort numbers by most recently created

--- server/routes/contacts/manage/contact-details/contactDetailsContorller.test.ts
+++ server/routes/contacts/manage/contact-details/contactDetailsContorller.test.ts
@@ -56,14 +56,28 @@ describe('GET /contacts/manage/:contactId', () => {
     })
   })
   describe('phone numbers', () => {
-    it('should render phone numbers', async () => {
+    it('should render phone numbers in reverse created date order', async () => {
       auditService.logPageView.mockResolvedValue(null)
       prisonerSearchService.getByPrisonerNumber.mockResolvedValue(TestData.prisoner())
       contactsService.getContact.mockResolvedValue(
         TestData.contact({
           phoneNumbers: [
-            TestData.getContactPhoneNumberDetails('MOB', 'Mobile phone', '07878 111111', 1),
-            TestData.getContactPhoneNumberDetails('HOME', 'Home phone', '01111 777777', 2),
+            TestData.getContactPhoneNumberDetails(
+              'MOB',
+              'Mobile phone',
+              '07878 111111',
+              1,
+              '012',
+              '2024-10-04T09:30:00.000000',
+            ),
+            TestData.getContactPhoneNumberDetails(
+              'HOME',
+              'Home phone',
+              '01111 777777',
+              2,
+              '321',
+              '2024-10-04T10:30:00.000000',
+            ),
           ],
         }),
       )
@@ -74,8 +88,14 @@ describe('GET /contacts/manage/:contactId', () => {
       // Then
       const $ = cheerio.load(response.text)
       expect(response.status).toEqual(200)
+
       expect($('.phone-number-value-1').text().trim()).toStrictEqual('07878 111111')
       expect($('.phone-number-value-2').text().trim()).toStrictEqual('01111 777777')
+
+      const phoneNumbers = $('.phone-number-value').toArray()
+      expect(phoneNumbers).toHaveLength(2)
+      expect($(phoneNumbers[0]).text().trim()).toStrictEqual('01111 777777')
+      expect($(phoneNumbers[1]).text().trim()).toStrictEqual('07878 111111')
     })
 
     it('should render not provided if no phone numbers', async () => {
