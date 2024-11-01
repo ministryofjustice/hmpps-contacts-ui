52f60ec304ab88976b6cfd8987ce7beb930ee8d5
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-11-01 12:16:36 +0000

    Integration test for domestic status starting from Manage contact

--- integration_tests/e2e/selectDomesticStatus.cy.ts
+++ integration_tests/e2e/selectDomesticStatus.cy.ts
@@ -1,9 +1,7 @@
 import { components } from '../../server/@types/contactsApi'
 import TestData from '../../server/routes/testutils/testData'
-import ListContactsPage from '../pages/listContacts'
 import ManageContactDetailsPage from '../pages/manageContactDetails'
 import Page from '../pages/page'
-import SearchPrisonerPage from '../pages/searchPrisoner'
 import SelectDomesticStatusPage from '../pages/selectDomesticStatusPage'
 
 export type PatchContactRequest = components['schemas']['PatchContactRequest']
@@ -11,24 +9,19 @@ export type PatchContactRequest = components['schemas']['PatchContactRequest']
 context('Select Domestic Status', () => {
   const contactId = 22
   beforeEach(() => {
-    const { prisonerNumber } = TestData.prisoner()
     cy.task('reset')
-    cy.task('stubSignIn', { roles: ['PRISON'] })
-    cy.signIn()
     cy.task('stubComponentsMeta')
-    cy.task('stubPrisoners', {
-      results: {
-        totalPages: 1,
-        totalElements: 1,
-        content: [TestData.prisoner()],
-      },
-      prisonId: 'HEI',
-      term: prisonerNumber,
-    })
+    cy.task('stubSignIn', { roles: ['PRISON'] })
+    cy.task('stubPhoneTypeReferenceData')
+    cy.task('stubTitlesReferenceData')
     cy.task('stubPrisonerById', TestData.prisoner())
     cy.task('stubGetContactById', TestData.contact())
-    cy.task('stubContactList', 'A1234BC')
-    cy.visit('/contacts/manage/prisoner-search/start')
+
+    cy.signIn()
+    const { prisonerNumber } = TestData.prisoner()
+    cy.visit(`/prisoner/${prisonerNumber}/contacts/manage/${contactId}`)
+
+    Page.verifyOnPage(ManageContactDetailsPage, 'Jones Mason')
   })
 
   it(`should render manage contact details domestic status`, () => {
@@ -36,13 +29,10 @@ context('Select Domestic Status', () => {
       domesticStatus: 'S',
       updatedBy: 'USER1',
     }
-    const { prisonerNumber } = TestData.prisoner()
     cy.task('stubTitlesReferenceData')
     cy.task('stubGetDomesticStatuses')
     cy.task('stubPatchContactById', { contactId, request })
 
-    Page.verifyOnPage(SearchPrisonerPage).enterPrisoner(prisonerNumber).clickSearchButton().clickPrisonerLink('A1234BC')
-    Page.verifyOnPage(ListContactsPage).clickContactNamesLink(22)
     Page.verifyOnPage(ManageContactDetailsPage).clickChangeDomesticStatusLink()
     Page.verifyOnPage(SelectDomesticStatusPage, 'Jones Mason')
       .selectDomesticStatus('Single-not married/in civil partnership')
