59477124c2fa29c958d19d4eab61aa7ed39dd083
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2024-11-01 17:17:05 +0000

    PRC-169 : Manage contact staff status - fix back link

--- server/routes/contacts/manage/staff/manageContactStaffController.test.ts
+++ server/routes/contacts/manage/staff/manageContactStaffController.test.ts
@@ -17,7 +17,6 @@ const contactsService = new ContactsService(null) as jest.Mocked<ContactsService
 
 let app: Express
 const prisonerNumber = 'A1234BC'
-const contactId = '10'
 
 beforeEach(() => {
   app = appWithAllRoutes({
@@ -39,18 +38,28 @@ describe('GET /prisoner/:prisonerNumber/contacts/manage/:contactId/staff', () =>
     ['YES', { isStaff: true }],
     ['NO', { isStaff: false }],
   ])('should render manage staff page when isStaff is %p', async (isStaff, expectedResponse) => {
+    const getContactResponse = TestData.contact(expectedResponse)
+    const contactId = getContactResponse.id
     // Given
     auditService.logPageView.mockResolvedValue(null)
     prisonerSearchService.getByPrisonerNumber.mockResolvedValue(TestData.prisoner())
-    contactsService.getContact.mockResolvedValue(TestData.contact(expectedResponse))
+    contactsService.getContact.mockResolvedValue(getContactResponse)
 
     // When
+
     const response = await request(app).get(`/prisoner/${prisonerNumber}/contacts/manage/${contactId}/staff`)
     const $ = cheerio.load(response.text)
 
     // Then
     expect(response.status).toEqual(200)
     expect($('input[type=radio]:checked').val()).toStrictEqual(isStaff)
+    expect($('[data-qa=cancel-button]').first().attr('href')).toStrictEqual(
+      `/prisoner/${prisonerNumber}/contacts/manage/${contactId}`,
+    )
+    expect($('[data-qa=back-link]').first().attr('href')).toStrictEqual(
+      `/prisoner/${prisonerNumber}/contacts/manage/${contactId}`,
+    )
+
     expect(auditService.logPageView).toHaveBeenCalledWith(Page.MANAGE_CONTACT_UPDATE_STAFF_PAGE, {
       who: user.username,
       correlationId: expect.any(String),
@@ -59,6 +68,7 @@ describe('GET /prisoner/:prisonerNumber/contacts/manage/:contactId/staff', () =>
 })
 
 describe('POST /prisoner/:prisonerNumber/contacts/manage/:contactId/staff', () => {
+  const contactId = '10'
   describe('update contact with staff status', () => {
     test.each([
       ['YES', { isStaff: true, updatedBy: 'id' }],
