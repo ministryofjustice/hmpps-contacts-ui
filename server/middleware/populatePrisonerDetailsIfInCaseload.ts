858af079cd00b2ddf8a0dec611d289da64c9970d
James Baker <james.baker3@digital.justice.gov.uk>
2024-11-12 16:42:07 +0000

    PRC-258: Audit when prisoner not in caseload

--- server/middleware/populatePrisonerDetailsIfInCaseload.ts
+++ server/middleware/populatePrisonerDetailsIfInCaseload.ts
@@ -1,19 +1,35 @@
 import { Request, RequestHandler } from 'express'
-import { PrisonerSearchService } from '../services'
+import { AuditService, PrisonerSearchService } from '../services'
 import { Prisoner } from '../data/prisonerOffenderSearchTypes'
 import asyncMiddleware from './asyncMiddleware'
 import PrisonerDetails = journeys.PrisonerDetails
 
-const populatePrisonerDetailsIfInCaseload = (prisonerSearchService: PrisonerSearchService): RequestHandler => {
+const populatePrisonerDetailsIfInCaseload = (
+  prisonerSearchService: PrisonerSearchService,
+  auditService: AuditService,
+): RequestHandler => {
   return asyncMiddleware(async (req: Request<{ prisonerNumber: string }>, res, next) => {
     const { prisonerNumber } = req.params
     const { user } = res.locals
-    const prisoner = await prisonerSearchService.getByPrisonerNumber(prisonerNumber, user)
-    if (!req.session.prisonId || req.session.prisonId !== prisoner.prisonId) {
-      return res.render('pages/errors/notFound')
-    }
-    res.locals.prisonerDetails = toPrisonerDetails(prisoner)
-    return next()
+    return prisonerSearchService.getByPrisonerNumber(prisonerNumber, user).then((prisoner: Prisoner) => {
+      if (!req.session.prisonId || req.session.prisonId !== prisoner.prisonId) {
+        auditService.logAuditEvent({
+          what: 'NOT_IN_CASELOAD',
+          who: user.username,
+          correlationId: req.id,
+          subjectType: 'PRISONER_NUMBER',
+          subjectId: prisonerNumber,
+          details: {
+            userCurrentPrison: req.session.prisonId,
+            prisonerCurrentPrison: prisoner.prisonId,
+          },
+        })
+        return res.render('pages/errors/notFound')
+      }
+
+      res.locals.prisonerDetails = toPrisonerDetails(prisoner)
+      return next()
+    })
   })
 
   function toPrisonerDetails(prisoner: Prisoner): PrisonerDetails {
--- server/routes/contacts/add/addContactRoutes.ts
+++ server/routes/contacts/add/addContactRoutes.ts
@@ -48,7 +48,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/search/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, contactsSearchController),
     asyncMiddleware(contactsSearchController.GET),
   )
@@ -64,7 +64,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/add/confirmation/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, contactConfirmationController),
     asyncMiddleware(contactConfirmationController.GET),
   )
@@ -88,7 +88,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/enter-name/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, enterNameController),
     asyncMiddleware(enterNameController.GET),
   )
@@ -103,7 +103,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/select-relationship/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, selectRelationshipController),
     asyncMiddleware(selectRelationshipController.GET),
   )
@@ -118,7 +118,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/select-emergency-contact/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, selectEmergencyContact),
     asyncMiddleware(selectEmergencyContact.GET),
   )
@@ -133,7 +133,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/select-next-of-kin/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, selectNextOfKinController),
     asyncMiddleware(selectNextOfKinController.GET),
   )
@@ -148,7 +148,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/enter-dob/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, enterDobController),
     asyncMiddleware(enterDobController.GET),
   )
@@ -163,7 +163,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/enter-estimated-dob/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, enterEstimatedDobController),
     asyncMiddleware(enterEstimatedDobController.GET),
   )
@@ -178,7 +178,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/enter-relationship-comments/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, enterRelationshipCommentsController),
     asyncMiddleware(enterRelationshipCommentsController.GET),
   )
@@ -193,7 +193,7 @@ const AddContactRoutes = (
   router.get(
     '/prisoner/:prisonerNumber/contacts/create/check-answers/:journeyId',
     ensureInAddContactJourney(),
-    populatePrisonerDetailsIfInCaseload(prisonerSearchService),
+    populatePrisonerDetailsIfInCaseload(prisonerSearchService, auditService),
     logPageViewMiddleware(auditService, checkAnswersController),
     asyncMiddleware(checkAnswersController.GET),
   )
