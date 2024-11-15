a185cb05cc2b152cad3047c90202fbfbc46c5a5f
rosariodiaferia-moj <rosario.diaferia@digital.justice.gov.uk>
2024-11-15 14:13:27 +0000

    Validation schema added

---
+++ server/routes/contacts/manage/email-addresses/emailAddressesSchemas.test.ts
@@ -0,0 +1,63 @@
+import { deduplicateFieldErrors } from '../../../../middleware/validationMiddleware'
+import { emailSchemaFactory } from './emailSchemas'
+
+describe('emailSchemaFactory', () => {
+  type Form = {
+    email: string
+  }
+  const baseForm: Form = {
+    email: '',
+  }
+  describe('should validate a identity number form', () => {
+    const EMAIL_REQUIRED_ERROR_MESSAGE = `Enter the contact's email address`
+    const EMAIL_NUMBER_OF_CHARACTERS_LIMIT_ERROR_MESSAGE = `The contact's email address should be 240 characters or fewer`
+    const EMAIL_FORMAT_ERROR_MESSAGE = `Enter an email address in the correct format, like name@example.com`
+
+    it('should require email', async () => {
+      // Given
+      const form = { ...baseForm, email: '' }
+
+      // When
+      const result = await doValidate(form)
+
+      // Then
+      expect(result.success).toStrictEqual(false)
+      const deduplicatedFieldErrors = deduplicateFieldErrors(result)
+      expect(deduplicatedFieldErrors).toStrictEqual({
+        email: [EMAIL_FORMAT_ERROR_MESSAGE, EMAIL_REQUIRED_ERROR_MESSAGE],
+      })
+    })
+
+    it.each([
+      ['name@', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['@example', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['@example.com', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['name_example.com', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['name_example.co%m', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['name@.com', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['*^$$@example.com', EMAIL_FORMAT_ERROR_MESSAGE],
+      ['name@example.22com', EMAIL_FORMAT_ERROR_MESSAGE],
+      [''.padEnd(40, '1'), EMAIL_FORMAT_ERROR_MESSAGE],
+      [
+        `LoOremipsumdolorsitametconsecteturadipiscingelitCurabiturposuereimperdietmollisFusceidfinibusvelit@DonecposueretellusetjustocommodointemporipsumvariusNullaplacerattemporvolutpatIndignissimveliteujustoLoremipsumdolorsitametconsecteturacer.com`,
+        EMAIL_NUMBER_OF_CHARACTERS_LIMIT_ERROR_MESSAGE,
+      ],
+    ])('should require a valid email', async (email: string, erroMessage: string) => {
+      // Given
+      const form = { ...baseForm, email }
+
+      // When
+      const result = await doValidate(form)
+
+      // Then
+      expect(result.success).toStrictEqual(false)
+      const deduplicatedFieldErrors = deduplicateFieldErrors(result)
+      expect(deduplicatedFieldErrors).toStrictEqual({ email: [erroMessage] })
+    })
+
+    const doValidate = async (form: Form) => {
+      const schema = await emailSchemaFactory()()
+      return schema.safeParse(form)
+    }
+  })
+})
---
+++ server/routes/contacts/manage/email-addresses/emailSchemas.ts
@@ -0,0 +1,24 @@
+import z from 'zod'
+import { createSchema } from '../../../../middleware/validationMiddleware'
+
+const EMAIL_REQUIRED_ERROR_MESSAGE = `Enter the contact's email address`
+const EMAIL_NUMBER_OF_CHARACTERS_LIMIT_ERROR_MESSAGE = `The contact's email address should be 240 characters or fewer`
+const EMAIL_FORMAT_ERROR_MESSAGE = `Enter an email address in the correct format, like name@example.com`
+
+export const emailSchemaFactory = () => async () => {
+  return createSchema({
+    email: z
+      .string({ message: EMAIL_REQUIRED_ERROR_MESSAGE })
+      // .min(1, {message: EMAIL_REQUIRED_ERROR_MESSAGE})
+      .email({ message: EMAIL_FORMAT_ERROR_MESSAGE })
+      .max(240, EMAIL_NUMBER_OF_CHARACTERS_LIMIT_ERROR_MESSAGE)
+      .refine(val => val?.trim().length > 0, { message: EMAIL_REQUIRED_ERROR_MESSAGE }),
+  })
+  // .superRefine((val, ctx) => {
+  //   if (val.email) {
+  //       ctx.addIssue({ code: z.ZodIssueCode.custom, message: EMAIL_REQUIRED_ERROR_MESSAGE, path: ['email'] })
+  //   }
+  // })
+}
+
+export type EmailSchemaType = z.infer<Awaited<ReturnType<ReturnType<typeof emailSchemaFactory>>>>
