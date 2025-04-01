698e24143e81c533c277b6c6de89ae7ec93d64c1
James Baker <james.baker3@digital.justice.gov.uk>
2025-04-01 11:31:16 +0100

    PRC-553: Back link tweaks
    Added some missing custom back link labels and restored the behaviour of back links on employments and address interstitial pages.

--- integration_tests/e2e/createContactWithEmployments.cy.ts
+++ integration_tests/e2e/createContactWithEmployments.cy.ts
@@ -134,7 +134,7 @@ context('Create Contact With Addresses', () => {
       .clickLinkTo('Employers', UpdateEmploymentsPage, true)
       .clickButtonTo('Continue', AddContactAdditionalInfoPage, 'First Last')
       .clickLinkTo('Employers', UpdateEmploymentsPage, true)
-      .clickLink('Back')
+      .clickLink('Back to additional information options')
 
     // Can enter some employment records
     Page.verifyOnPage(AddContactAdditionalInfoPage, 'First Last') //
