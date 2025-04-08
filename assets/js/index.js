aae8d4980dda040827788a083c6ddcce1ee872f0
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-04-08 14:40:51 +0100

    fix moj filter

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,7 @@
-import $ from 'jquery'
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 import * as ContactsFrontend from './all'
 
-window.$ = $
 govukFrontend.initAll()
 mojFrontend.initAll()
 ContactsFrontend.initAll()
