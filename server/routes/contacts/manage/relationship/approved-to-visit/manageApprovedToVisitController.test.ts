3cd10e5ce67ec7b7ec3c59d2d81ec9560af5274a
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2025-07-15 12:21:28 +0100

    PRC-828 : Change to existing “Add visits approval” add hint text to the screen after the “Skip this question"

--- server/routes/contacts/manage/relationship/approved-to-visit/manageApprovedToVisitController.test.ts
+++ server/routes/contacts/manage/relationship/approved-to-visit/manageApprovedToVisitController.test.ts
@@ -74,6 +74,8 @@ describe('GET /prisoner/:prisonerNumber/contacts/manage/:contactId/relationship/
     )
     expect($('[data-qa=breadcrumbs]')).toHaveLength(0)
     expect($('[data-qa=continue-button]').first().text().trim()).toStrictEqual('Confirm and save')
+    // Assert hint text and details are NOT present
+    expect($('.govuk-hint').length).toBe(0)
 
     expect(auditService.logPageView).toHaveBeenCalledWith(Page.MANAGE_CONTACT_UPDATE_APPROVED_TO_VISIT_PAGE, {
       who: authorisingUser.username,
