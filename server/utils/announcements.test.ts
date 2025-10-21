117320c05fe9247afd18add03bea5de1797457b1
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2025-10-21 12:31:02 +0100

    PRC-965 : update guidance text (#611)
    Co-authored-by: prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>

--- server/utils/announcements.test.ts
+++ server/utils/announcements.test.ts
@@ -10,7 +10,7 @@ describe('announcement', () => {
       // Arrange
       const expectedAnnouncement = {
         title: 'Your prison has the new Contacts service in DPS.',
-        html: `You can access guidance and demo videos on our <a class="govuk-notification-banner__link" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/Managing%20Prisoner%20Contacts.aspx?csf=1&web=1&e=47P78C" target="_blank" rel="noopener noreferrer">SharePoint site</a>. If you think you need a different role, read this article, and speak to your LSA. The equivalent NOMIS functionality will be disabled in the coming months with read-only access. If you have any questions, contact <a class="govuk-notification-banner__link" href="mailto:managingcontacts@justice.gov.uk">managingcontacts@justice.gov.uk</a>`,
+        html: `You can access guidance and demo videos on our <a class="govuk-notification-banner__link" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/Managing%20Prisoner%20Contacts.aspx?csf=1&web=1&e=47P78C" target="_blank" rel="noopener noreferrer">SharePoint site</a>. If you think you need a different role, speak to your LSA. The equivalent NOMIS functionality will be disabled in the coming months with read-only access. If you have any questions, contact <a class="govuk-notification-banner__link" href="mailto:managingcontacts@justice.gov.uk">managingcontacts@justice.gov.uk</a>`,
       }
       // Act & Assert
       expect(getAnnouncement()).toMatchObject(expectedAnnouncement)
