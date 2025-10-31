317f6d3274f17570c8a49a3d16dd0e94e45a2649
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2025-10-31 13:10:50 +0000

    PRC-981 : fix comments after team review (#621)
    * PRC-981 : fix comments after team review
    
    * PRC-981 : fix comments after team review
    
    ---------
    
    Co-authored-by: prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>

--- server/testutils/testPrisonerAlertsData.ts
+++ server/testutils/testPrisonerAlertsData.ts
@@ -1,29 +1,36 @@
-import { PageAlert } from '../data/alertsApiTypes'
+export default function pagedPrisonerAlertsData(
+  {
+    prisonNumber = 'G5276UE',
+    createdByDisplayName = 'Azfgorigh Johneh',
+    lastModifiedByDisplayName = 'Admin&onb Cnomis',
+    ...alertOverrides
+  } = {},
+  overrides = {},
+) {
+  const alert = {
+    alertUuid: '1',
+    prisonNumber,
+    alertCode: {
+      alertTypeCode: 'X',
+      alertTypeDescription: 'Hold or transfer restriction',
+      code: 'XVL',
+      description: 'Accredited Programme hold',
+    },
+    description: 'gzmONOQRHANxDCBhAmycaZSDIfUTkIlYdGXcdhtVJLBgzmONOQRHANxDCBhAmycaZSDIfUTkIlYdGXcdhtVJL',
+    authorisedBy: 'OSYQOSYQ',
+    activeFrom: '2016-02-01',
+    isActive: true,
+    createdAt: '2015-09-25T13:56:34',
+    createdBy: 'DQU83Q',
+    createdByDisplayName,
+    lastModifiedAt: '2017-05-09T21:53:32',
+    lastModifiedBy: 'OMS_OWNER',
+    lastModifiedByDisplayName,
+    ...alertOverrides, // allows any other alert property to be overridden
+  }
 
-export default function pagedPrisonerAlertsData(overrides = {}): PageAlert {
   return {
-    content: [
-      {
-        alertUuid: '1',
-        prisonNumber: 'G5276UE',
-        alertCode: {
-          alertTypeCode: 'X',
-          alertTypeDescription: 'Hold or transfer restriction',
-          code: 'XVL',
-          description: 'Accredited Programme hold',
-        },
-        description: 'gzmONOQRHANxDCBhAmycaZSDIfUTkIlYdGXcdhtVJLBgzmONOQRHANxDCBhAmycaZSDIfUTkIlYdGXcdhtVJL',
-        authorisedBy: 'OSYQOSYQ',
-        activeFrom: '2016-02-01',
-        isActive: true,
-        createdAt: '2015-09-25T13:56:34',
-        createdBy: 'DQU83Q',
-        createdByDisplayName: 'Azfgorigh Johneh',
-        lastModifiedAt: '2017-05-09T21:53:32',
-        lastModifiedBy: 'OMS_OWNER',
-        lastModifiedByDisplayName: 'Admin&onb Cnomis',
-      },
-    ],
-    ...overrides,
+    content: [alert],
+    ...overrides, // allows overriding the top-level PageAlert fields (like pagination)
   }
 }
