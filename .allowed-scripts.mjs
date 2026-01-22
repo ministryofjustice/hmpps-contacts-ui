f484a500777d6f43447d3403533a64dc85331103
sandeep.khajuria <sankhajuria@gmail.com>
2026-01-22 16:54:52 +0000

    PRC-1064 - incorporate the allow-list NPM dependency

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -12,8 +12,6 @@ export default configureAllowedScripts({
     'node_modules/esbuild@0.27.0': 'ALLOW',
     // Needed by jest for running tests in watch mode
     'node_modules/fsevents@2.3.3': 'ALLOW',
-    // Needed by playwright for detecting file system changes during test runs
-    'node_modules/playwright/node_modules/fsevents@2.3.2': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
     'node_modules/unrs-resolver@1.11.1': 'ALLOW',
   },
