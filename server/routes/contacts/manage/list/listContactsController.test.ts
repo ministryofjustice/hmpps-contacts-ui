c8c4a4208bfcfa556b144c6fa3e8b60c04810337
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2026-01-28 13:08:35 +0000

    PRC-986 :remove feature flag for pilot banner (#687)
    * PRC-986 :remove feature flag for pilot banner
    
    * PRC-986 :remove feature flag for pilot banner
    
    ---------
    
    Co-authored-by: prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>

--- server/routes/contacts/manage/list/listContactsController.test.ts
+++ server/routes/contacts/manage/list/listContactsController.test.ts
@@ -720,20 +720,12 @@ describe('listContactsController', () => {
       expect($('#flagsNextOfKin').attr('checked')).toStrictEqual('checked')
     })
 
-    it('should render the NOMIS off announcement banner for when a prison is in nomis off pilot list', async () => {
+    it('should render the NOMIS off announcement banner', async () => {
       // Given
       contactsService.filterPrisonerContacts.mockResolvedValue({
         content: [minimalContact],
         page: { totalElements: 1, totalPages: 1, size: 10, number: 0 },
       })
-      const featureEnabledPrisonId = 'WWI' // This is in the featureEnabledPrisons set
-
-      // Mock the prisoner search service to return a prisoner with a feature-enabled prison ID
-      prisonerSearchService.getByPrisonerNumber.mockResolvedValueOnce({
-        ...prisoner,
-        prisonId: featureEnabledPrisonId,
-      })
-      process.env['FEATURE_NOMIS_SCREENS_OFF_PRISONS'] = 'KMI,GNI,SPI,LGI,DWI,HOI,WWI'
       // When
       const response = await request(app).get(`/prisoner/${prisonerNumber}/contacts/list`)
 
@@ -741,27 +733,7 @@ describe('listContactsController', () => {
       const $ = cheerio.load(response.text)
       const banner = $('.moj-alert__heading')
       expect(banner.length).toBe(1)
-      expect(banner.text()).toContain('Your prison has the new Contacts service in DPS.')
-      const bannerLink = $('.govuk-notification-banner__link')
-      expect(bannerLink.html()).toContain('managingcontacts@justice.gov.uk')
-    })
-
-    it('should render the announcement banner for all other prisons when they are not in nomis off pilot list', async () => {
-      // Given
-      contactsService.filterPrisonerContacts.mockResolvedValue({
-        content: [minimalContact],
-        page: { totalElements: 1, totalPages: 1, size: 10, number: 0 },
-      })
-      process.env['FEATURE_NOMIS_SCREENS_OFF_PRISONS'] = 'KMI,GNI,SPI,LGI,DWI,HOI,WWI'
-
-      // When
-      const response = await request(app).get(`/prisoner/${prisonerNumber}/contacts/list`)
-
-      // Then
-      const $ = cheerio.load(response.text)
-      const banner = $('.moj-alert__heading')
-      expect(banner.length).toBe(1)
-      expect(banner.text()).toContain('Your prison has the new Contacts service in DPS.')
+      expect(banner.text()).toContain('You must use DPS to manage contacts in your prison')
       const bannerLink = $('.govuk-notification-banner__link')
       expect(bannerLink.html()).toContain('managingcontacts@justice.gov.uk')
     })
--- server/utils/announcement.ts
+++ server/utils/announcement.ts
@@ -3,26 +3,12 @@ interface Announcement {
   html: string
 }
 
-const getEnabledPrisons = () => {
-  return new Set(
-    (process.env['FEATURE_NOMIS_SCREENS_OFF_PRISONS'] || '')
-      .split(',')
-      .map(code => code.trim())
-      .filter(Boolean),
-  )
-}
-
 const NOMIS_OFF_PILOT_ANNOUNCEMENT: Announcement = {
   title: 'You must use DPS to manage contacts in your prison',
   html: `The equivalent NOMIS functionality has now been switched-off. You can access guidance and demo videos on <br/>our <a class="govuk-notification-sharepoint__link" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/Managing%20Prisoner%20Contacts.aspx?csf=1&web=1&e=47P78C" target="_blank" rel="noopener noreferrer">SharePoint site</a>. If you have any questions or feedback, email 
 <a class="govuk-notification-banner__link" href="mailto:managingcontacts@justice.gov.uk">managingcontacts@justice.gov.uk</a>.`,
 }
 
-const DEFAULT_ANNOUNCEMENT: Announcement = {
-  title: 'Your prison has the new Contacts service in DPS.',
-  html: `You can access guidance and demo videos on our <a class="govuk-notification-sharepoint__link" href="https://justiceuk.sharepoint.com/:u:/r/sites/prisons-digital/SitePages/Managing%20Prisoner%20Contacts.aspx?csf=1&web=1&e=47P78C" target="_blank" rel="noopener noreferrer">SharePoint site</a>. If you think you need a different role, speak to your LSA. The equivalent NOMIS functionality will be disabled in the coming months with read-only access. If you have any questions, contact <a class="govuk-notification-banner__link" href="mailto:managingcontacts@justice.gov.uk">managingcontacts@justice.gov.uk</a>`,
-}
-
-export function getAnnouncement(prison: string): Announcement | undefined {
-  return getEnabledPrisons().has(prison) ? NOMIS_OFF_PILOT_ANNOUNCEMENT : DEFAULT_ANNOUNCEMENT
+export function getAnnouncement(): Announcement {
+  return NOMIS_OFF_PILOT_ANNOUNCEMENT
 }
