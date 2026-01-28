79ac9eefbf6e2d34f02081dd58ecf3877077f3a7
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2026-01-28 12:27:33 +0000

    Prc 1017 fix page title (#698)
    * PRC-1017 : fix page title, unit tests
    
    * PRC-1017 : fix page title, unit tests
    
    * PRC-1017 : fix page card background to match ccip
    
    ---------
    
    Co-authored-by: prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>

--- server/routes/index.test.ts
+++ server/routes/index.test.ts
@@ -1,4 +1,5 @@
 import type { Express } from 'express'
+import * as cheerio from 'cheerio'
 import request from 'supertest'
 import { appWithAllRoutes, basicPrisonUser } from './testutils/appSetup'
 import { MockedService } from '../testutils/mockedServices'
@@ -23,12 +24,33 @@ afterEach(() => {
 })
 
 describe('GET /', () => {
-  it('The entry point is contact list so default page should show a 404', async () => {
-    await request(app)
-      .get('/')
-      .expect(200)
-      .expect(res => {
-        expect(res.text).toContain('Contacts')
-      })
+  it('The entry point is contacts - home page', async () => {
+    const response = await request(app).get(`/`)
+
+    // Then
+    expect(response.status).toEqual(200)
+
+    const $ = cheerio.load(response.text)
+    expect($('title').text()).toStrictEqual(`Contacts - DPS`)
+    expect($('[data-qa=breadcrumbs]')).toHaveLength(1)
+    const breadcrumbs = $('[data-qa=breadcrumbs] a')
+    expect(breadcrumbs).toHaveLength(1)
+    expect(breadcrumbs.eq(0).text().trim()).toStrictEqual('Digital Prison Services')
+    expect(breadcrumbs.eq(0).attr('href')).toStrictEqual('http://localhost:3001')
+
+    const card1 = $('[data-qa=manage-contacts-card]')
+    expect(card1).toHaveLength(1)
+    const link1 = card1.find('a.card__link')
+    expect(link1.attr('href')).toStrictEqual('/start')
+    expect(link1.text().trim()).toStrictEqual('Search for a contact')
+    expect(card1.find('p.card__description').text().trim()).toStrictEqual("View a contact's details.")
+    const card2 = $('[data-qa=how-to-manage-prisoner-contacts-card]')
+    expect(card2).toHaveLength(1)
+    const link2 = card2.find('a.card__link')
+    expect(link2.attr('href')).toStrictEqual('/how-to-manage-a-prisoners-contacts')
+    expect(link2.text().trim()).toStrictEqual("How to manage a prisoner's contacts")
+    expect(card2.find('p.card__description').text().trim()).toStrictEqual(
+      "Information on how to manage a prisoner's contacts.",
+    )
   })
 })
