8b901b2325ea93a1a9420f24538a602b1773c664
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2026-02-05 16:05:25 +0000

    PRC-1006 : Sortable table columns announced unclearly (Medium)

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,16 +1,9 @@
 import { nodeListForEach } from './utils'
 import AutoComplete from './autocomplete'
 
-const $upArrow = `<svg width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M6.5625 15.5L11 6.63125L15.4375 15.5H6.5625Z" fill="currentColor"/>
-</svg>`
-const $downArrow = `<svg width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M15.4375 7L11 15.8687L6.5625 7L15.4375 7Z" fill="currentColor"/>
-</svg>`
-const $upDownArrow = `<svg width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8.1875 9.5L10.9609 3.95703L13.7344 9.5H8.1875Z" fill="currentColor"/>
-<path d="M13.7344 12.0781L10.9609 17.6211L8.1875 12.0781H13.7344Z" fill="currentColor"/>
-</svg>`
+const $upArrow = `<svg width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.5625 15.5L11 6.63125L15.4375 15.5H6.5625Z" fill="currentColor"/></svg>`
+const $downArrow = `<svg width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.4375 7L11 15.8687L6.5625 7L15.4375 7Z" fill="currentColor"/></svg>`
+const $upDownArrow = `<svg width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.1875 9.5L10.9609 3.95703L13.7344 9.5H8.1875Z" fill="currentColor"/><path d="M13.7344 12.0781L10.9609 17.6211L8.1875 12.0781H13.7344Z" fill="currentColor"/></svg>`
 
 function initAll() {
   var $autoCompleteElements = document.getElementsByName('autocompleteElements')
@@ -18,23 +11,35 @@ function initAll() {
     new AutoComplete($autoCompleteElements)
   })
 
+  // Update sortable table headings icons. Support both legacy markup (button inside anchor)
+  // and new markup (anchor-only with inline SVG). We only insert an icon when none exists.
   const $headings = Array.from(document.querySelectorAll('th'))
   for (const $heading of $headings) {
+    if (!$heading.hasAttribute('aria-sort')) continue
+
+    const direction = $heading.getAttribute('aria-sort')
+
+    // Prefer anchor if present, otherwise fall back to button (legacy)
+    const $anchor = $heading.querySelector('a')
     const $button = $heading.querySelector('button')
-    if ($heading.hasAttribute('aria-sort') && $button) {
-      var _$button$querySelector
-      const direction = $heading.getAttribute('aria-sort')
-      ;(_$button$querySelector = $button.querySelector('svg')) == null || _$button$querySelector.remove()
-      switch (direction) {
-        case 'ascending':
-          $button.insertAdjacentHTML('beforeend', $upArrow)
-          break
-        case 'descending':
-          $button.insertAdjacentHTML('beforeend', $downArrow)
-          break
-        default:
-          $button.insertAdjacentHTML('beforeend', $upDownArrow)
-      }
+
+    // target element we'll insert icon into
+    const $target = $anchor || $button
+    if (!$target) continue
+
+    // if there is already an svg inside target, remove it and re-insert the correct one
+    const existingSvg = $target.querySelector('svg')
+    if (existingSvg) existingSvg.remove()
+
+    switch (direction) {
+      case 'ascending':
+        $target.insertAdjacentHTML('beforeend', $upArrow)
+        break
+      case 'descending':
+        $target.insertAdjacentHTML('beforeend', $downArrow)
+        break
+      default:
+        $target.insertAdjacentHTML('beforeend', $upDownArrow)
     }
   }
 }
--- server/utils/convertToSortableColumns.ts
+++ server/utils/convertToSortableColumns.ts
@@ -8,7 +8,17 @@ export const convertToSortableColumns = (
   sort: string,
   hrefTemplate: string = '?sort={sortKey},{sortDirection}',
 ) => {
-  const [sortingKey, sortingDirection] = sort.split(',')
+  const [sortingKey, sortingDirection] = (sort || '').split(',')
+
+  const escapeHtml = (s: string) =>
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    String(s).replace(/[&<>"]/g, ch => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;' })[ch])
+
+  // Inline SVGs for icons (kept aria-hidden so screen readers rely on the visually-hidden text)
+  const upArrowSvg = `<svg class="pcl-sortable__icon" width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.5625 15.5L11 6.63125L15.4375 15.5H6.5625Z" fill="currentColor"></path></svg>`
+  const downArrowSvg = `<svg class="pcl-sortable__icon" width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.4375 7L11 15.8687L6.5625 7L15.4375 7Z" fill="currentColor"></path></svg>`
+  const upDownArrowSvg = `<svg class="pcl-sortable__icon" width="22" height="22" focusable="false" aria-hidden="true" role="img" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.1875 9.5L10.9609 3.95703L13.7344 9.5H8.1875Z" fill="currentColor"></path><path d="M13.7344 12.0781L10.9609 17.6211L8.1875 12.0781H13.7344Z" fill="currentColor"></path></svg>`
 
   return headings.map(heading => {
     const { text, key, ...others } = heading
@@ -16,13 +26,19 @@ export const convertToSortableColumns = (
       return heading
     }
 
+    const hrefAsc = escapeHtml(urlFromTemplate(hrefTemplate, key, 'asc'))
+    const hrefDesc = escapeHtml(urlFromTemplate(hrefTemplate, key, 'desc'))
+    const safeText = escapeHtml(text)
+
+    // active sorted column
     if (key === sortingKey) {
       if (sortingDirection === 'asc') {
         return {
           attributes: {
             'aria-sort': 'ascending',
           },
-          html: `<a href="${urlFromTemplate(hrefTemplate, key, 'desc')}"><button>${text}</button></a>`,
+          // Anchor-only control with accessible labels, visually-hidden hint, and an up-arrow icon to the right
+          html: `<a class="pcl-sortable" href="${hrefDesc}" aria-label="${safeText}, sorted ascending, activate to sort descending"><span class="pcl-sortable__label">${safeText}</span><span class="govuk-visually-hidden">, sorted ascending, activate to sort descending</span>${upArrowSvg}</a>`,
           ...others,
         }
       }
@@ -31,16 +47,20 @@ export const convertToSortableColumns = (
           attributes: {
             'aria-sort': 'descending',
           },
-          html: `<a href="${urlFromTemplate(hrefTemplate, key, 'asc')}"><button>${text}</button></a>`,
+          // Anchor-only control with a down-arrow icon to the right
+          html: `<a class="pcl-sortable" href="${hrefAsc}" aria-label="${safeText}, sorted descending, activate to sort ascending"><span class="pcl-sortable__label">${safeText}</span><span class="govuk-visually-hidden">, sorted descending, activate to sort ascending</span>${downArrowSvg}</a>`,
           ...others,
         }
       }
     }
+
+    // not currently sorted
     return {
       attributes: {
         'aria-sort': 'none',
       },
-      html: `<a href="${urlFromTemplate(hrefTemplate, key, 'asc')}"><button>${text}</button></a>`,
+      // Anchor-only control with combined up/down icon indicating sortable (icon on the right)
+      html: `<a class="pcl-sortable" href="${hrefAsc}" aria-label="${safeText}, sortable column, activate to sort ascending"><span class="pcl-sortable__label">${safeText}</span><span class="govuk-visually-hidden">, sortable column, activate to sort ascending</span>${upDownArrowSvg}</a>`,
       ...others,
     }
   })
