8de35544cc11841fd7cf7845b0c804e0b2154081
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-02-17 17:41:35 +0000

    VB-6371: Update environment names

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -44,7 +44,7 @@ export default function nunjucksSetup(app: express.Express): void {
   app.locals.asset_path = '/assets/'
   app.locals.applicationName = 'Contacts linked to a prisoner - DPS'
   app.locals.environmentName = config.environmentName
-  app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
+  app.locals.environmentNameColour = config.environmentName === 'preprod' ? 'govuk-tag--green' : ''
   let assetManifest: Record<string, string> = {}
 
   try {
