fa4abc12412b14e513d39086388203a754ccd782
Thomas McGowan <tpmcgowan@users.noreply.github.com>
2026-03-05 10:05:00 +0000

    Add save back link controller test

---
+++ server/routes/backLink/saveBackLinkController.test.ts
@@ -0,0 +1,66 @@
+import type { Express } from 'express'
+import request from 'supertest'
+import { SessionData } from 'express-session'
+import { appWithAllRoutes } from '../testutils/appSetup'
+import config from '../../config'
+import { UserBackLink } from './saveBackLinkController'
+
+jest.mock('../../services/auditService')
+
+let app: Express
+let session: Partial<SessionData>
+
+beforeEach(() => {
+  app = appWithAllRoutes({
+    sessionReceiver: (receivedSession: Partial<SessionData>) => {
+      session = receivedSession
+    },
+  })
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('GET /save-backlink', () => {
+  it('should save the backlink in session and redirect to the provided path in this service', async () => {
+    const backLinkQuery =
+      'service=prisoner-profile&' +
+      'returnPath=/prisoner/A1234BC/personal%23next-of-kin&' +
+      'redirectPath=/prisoner/A1234BC/contacts/manage/123/relationship/456'
+
+    const expectedRedirectPath = `${config.domain}/prisoner/A1234BC/contacts/manage/123/relationship/456`
+
+    const expectedReturnPath = `${config.serviceUrls.prisonerProfileUrl}/prisoner/A1234BC/personal#next-of-kin`
+
+    await request(app).get(`/save-backlink?${backLinkQuery}`).expect(302).expect('Location', expectedRedirectPath)
+
+    expect(session.userBackLink).toStrictEqual<UserBackLink>({
+      service: 'prisoner-profile',
+      url: expectedReturnPath,
+      prisonerContactId: '456',
+    })
+  })
+
+  it('should throw an error if required query parameters missing', async () => {
+    const backLinkQuery = 'service=prisoner-profile'
+
+    await request(app)
+      .get(`/save-backlink?${backLinkQuery}`)
+      .expect(500)
+      .expect(res => expect(res.text).toContain('Required query parameters missing'))
+
+    expect(session.userBackLink).toBeUndefined()
+  })
+
+  it('should throw an error if an unregistered back link service is specified', async () => {
+    const backLinkQuery = 'service=unregistered-service&returnPath=some-path&redirectPath=some-redirect'
+
+    await request(app)
+      .get(`/save-backlink?${backLinkQuery}`)
+      .expect(500)
+      .expect(res => expect(res.text).toContain('Unregistered service for back link'))
+
+    expect(session.userBackLink).toBeUndefined()
+  })
+})
