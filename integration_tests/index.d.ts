849b4e90f10e11812fe9f6a32fa88737f5f5843e
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-06-16 13:30:25 +0100

    Try to fix bad TypeScript

--- integration_tests/index.d.ts
+++ integration_tests/index.d.ts
@@ -2,8 +2,8 @@ declare namespace Cypress {
   interface Chainable {
     signIn(options?: { startUrl: string; failOnStatusCode?: boolean }): Chainable<AUTWindow>
 
-    verifyLastAPICall(matching: string | object, expected: object): Chainable<*>
+    verifyLastAPICall(matching: string | object, expected: object): Chainable<unknown>
 
-    verifyAPIWasCalled(matching: string | object, expected: number): Chainable<*>
+    verifyAPIWasCalled(matching: string | object, expected: number): Chainable<unknown>
   }
 }
