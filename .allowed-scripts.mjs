de16c67eb8b14fe55f94c45356425f84b36035b6
Paul Solecki <paul.solecki@digital.justice.gov.uk>
2026-07-28 11:31:26 +0100

    Update dependencies 2026-07-28

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,9 +3,9 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     // Needed by esbuild for watching files during development
-    'node_modules/@parcel/watcher@2.5.6': 'ALLOW',
+    'node_modules/@parcel/watcher@2.6.0': 'ALLOW',
     // Needed by Cypress for pulling in the Cypress binary
-    'node_modules/cypress@^15.14.0': 'ALLOW',
+    'node_modules/cypress@15.19.0': 'ALLOW',
     // Provides native integration, supporting the ability to write dtrace probes for bunyan
     'node_modules/dtrace-provider@^0.8.8': 'FORBID',
     // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
