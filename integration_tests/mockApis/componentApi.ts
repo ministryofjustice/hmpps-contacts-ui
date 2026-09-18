55cf6cda72658d4e0e3f86a79165402beff74576
Paul Solecki <51918433+psoleckimoj@users.noreply.github.com>
2026-09-18 17:04:40 +0100

    Switch to urlPattern for dps component lib test (#865)

--- integration_tests/mockApis/componentApi.ts
+++ integration_tests/mockApis/componentApi.ts
@@ -37,7 +37,7 @@ const stubComponentsMeta = () => {
   return stubFor({
     request: {
       method: 'GET',
-      url: '/components?component=header&component=footer&library=6.3.5',
+      urlPattern: `/components\\?component=header\\&component=footer\\&library=.*`,
     },
     response: {
       status: 200,
